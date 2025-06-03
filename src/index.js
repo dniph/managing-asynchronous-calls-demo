4a4774846d420f8c2f172e682ab900f633b05b33
Dennif <dennif.pa@gmail.com>
2025-06-02 17:11:44 -0700

    seven wonders call  implemented

--- src/index.js
+++ src/index.js
@@ -5,71 +5,13 @@ dotEnv.config(); // Load variables from .env
 const LOCATIONIQ_KEY = process.env.API_KEY; // Access the API_KEY from .env file
 
 
-/////////////////////////////////
-//    Initial Implementation   //
-/////////////////////////////////
-
-const findLatitudeAndLongitude = (query) => {
-  let latitude, longitude;
-  axios.get('https://us1.locationiq.com/v1/search.php',
-    {
-      params: {
-        key: LOCATIONIQ_KEY,
-        q: query,
-        format: 'json'
-      }
-    })
-    .then( (response) => {
-      latitude = response.data[0].lat;
-      longitude = response.data[0].lon;
-      console.log('success in findLatitudeAndLongitude!', latitude, longitude);
-    })
-    .catch( (error) => {
-      console.log('error in findLatitudeAndLongitude!');
-      // console.log(error); // If we want to see more info about the issue
-    });
-
-  return {
-    seattleLat: latitude,
-    seattleLon: longitude
-  };
-};
-
-const findLocation = (latitude, longitude) => {
-  axios.get('https://us1.locationiq.com/v1/reverse.php',
-    {
-      params: {
-        key: LOCATIONIQ_KEY,
-        format: 'json',
-        lat: latitude,
-        lon: longitude
-      }
-    })
-    .then( (response) => {
-      console.log('success in findLocation!', response.data);
-      return response.data;
-    })
-    .catch( (error) => {
-      console.log('error in findLocation!');
-      // console.log(error); // If we want to see more info about the issue
-    });
-};
-
-const seattleCoordinates = findLatitudeAndLongitude('Seattle, Washington, USA');
-const locations = findLocation(seattleCoordinates.seattleLat, seattleCoordinates.seattleLon);
-console.log(locations);
-
-
-/////////////////////////////////
-// Refactoring: Utilize `then` //
-/////////////////////////////////
 
 // const getLocationFromQuery = (query) => {
 //   // Make the first API call to get latitude and longitude
 //   findLatitudeAndLongitude(query)
 //     .then((response) => {
 //       // `response` is the data returned from the findLatitudeAndLongitude promise
-//
+
 //       // Make the next API call here, where we can use the `response` data from the previous call
 //       findLocation(response.latitude, response.longitude);
 //     })
@@ -124,3 +66,59 @@ console.log(locations);
 // };
 
 // getLocationFromQuery('Seattle, Washington, USA');
+
+
+// Lista de maravillas
+const wonders = [
+  "Great Wall of China",
+  "Petra, Jordan",
+  "Colosseum, Rome",
+  "Chichen Itza, Mexico",
+  "Machu Picchu, Peru",
+  "Taj Mahal, India",
+  "Christ the Redeemer, Brazil"
+];
+
+// Función para pausar
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+// Buscar lat y lon
+const findLatitudeAndLongitude = async (query) => {
+  try {
+    const response = await axios.get('https://us1.locationiq.com/v1/search.php', {
+      params: {
+        key: LOCATIONIQ_KEY,
+        q: query,
+        format: 'json'
+      }
+    });
+
+    const latitude = response.data[0].lat;
+    const longitude = response.data[0].lon;
+    console.log(`✔ ${query}: ${latitude}, ${longitude}`);
+    return { latitude, longitude };
+
+  } catch (error) {
+    console.error(`❌ Error fetching: ${query}`);
+    return { latitude: null, longitude: null };
+  }
+};
+
+// Función principal que itera con pausa
+const getWondersCoordinates = async () => {
+  const result = {};
+
+  for (let i = 0; i < wonders.length; i++) {
+    const place = wonders[i];
+    const coords = await findLatitudeAndLongitude(place);
+    result[place] = coords;
+
+    // Esperar 500ms entre llamadas
+    await sleep(500);
+  }
+
+  console.log("\n🌍 Coordenadas de las 7 Maravillas:");
+  console.log(JSON.stringify(result, null, 2));
+};
+
+getWondersCoordinates();
